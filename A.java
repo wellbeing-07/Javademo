bd99566977cd0add18a7511922aa53d1dcc83ad7
Kalyan Dhawale <dhawalekalyan78@gmail.com>
2024-04-08 23:56:55 +0530

    second commit

--- A.java
+++ A.java
@@ -2,12 +2,12 @@ package app_java_1;
 
 public class A {
 	public static void main(String[] args) {
-		var x= 10;
-		var y =20.8;
-		
+		int x= 10;
+		int y =20.8;
+		intg z=x +y;
 		System.out.println(x);
 		System.out.println(y);
-	
+		System.out.println(z)
 }
 
 }
